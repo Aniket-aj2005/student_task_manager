7c5749432eb1105bfef893953bffcb7ff827b2ff
Aniket-aj2005 <aniketjambhale5566@gmail>
2026-06-04 18:30:42 +0530

    Added SSL for TiDB

--- db_config.py
+++ db_config.py
@@ -2,15 +2,15 @@ import mysql.connector
 
 def get_database_connection():
     connection = mysql.connector.connect(
-        host = 'localhost',
-        user = 'root',
-        password = 'aniket#$123',
-        database = 'student_tasks_manager',
-        port = 4000
+        host='gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+        user='LBHdQuEK4ZhZoAq.root',
+        password='rU05KPedb2BNslDC',
+        database='student_tasks_manager',
+        port=4000,
+        ssl_disabled=False
     )
-    
-    return connection
 
+    return connection
 
 
 
