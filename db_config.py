d44de74caec8e0afe851a46599f097d589299adf
Aniket-aj2005 <aniketjambhale5566@gmail>
2026-06-04 18:45:04 +0530

    Fixed database name

--- db_config.py
+++ db_config.py
@@ -5,7 +5,7 @@ def get_database_connection():
         host = 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
         user = 'LBHdQuEK4ZhZoAq.root',
         password = 'rU05KPedb2BNslDC',
-        database = 'student_tasks_manager',
+        database = 'student_task_manager',
         port = 4000
     )
     
